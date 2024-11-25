62d69d888bae3f219f0816d4de0e03e0023a2680
Emanuel <emanuel.lezcano@ovnicore.com>
2024-11-25 18:51:59 -0300

    Null a campos que no tienen nada en get usuarios.

--- internal/dtos/roles_dto.go
+++ internal/dtos/roles_dto.go
@@ -4,8 +4,8 @@ type RolesDto struct {
 	ID          int64
 	Rol         string
 	Description string
-	Permissions []PermissionsDto
-	CreatedAt   string `json:"CreatedAt,omitempty"`
-	UpdatedAt   string `json:"UpdatedAt,omitempty"`
-	DeletedAt   string `json:"DeletedAt,omitempty"`
+	Permissions []PermissionsDto `json:"Permissions,omitempty"`
+	CreatedAt   string           `json:"CreatedAt,omitempty"`
+	UpdatedAt   string           `json:"UpdatedAt,omitempty"`
+	DeletedAt   string           `json:"DeletedAt,omitempty"`
 }
